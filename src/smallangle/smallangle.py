c46c9a345c5da4203d922649267f6fd4c0769e7a
JamesPunter <james.punter@student.uva.nl>
2024-11-15 15:52:33 +0100

    Smallangle with docstrings
    Docstrings added to smallangle.py. With command ''smallangle --help'' explanation for ''def function_group'', ''def sin'' and ''def tan'' are given. With "smallangle sin --help" explanation of option is given.

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -6,9 +6,13 @@ from numpy import pi
 
 @click.group()
 def function_group():
+    """Calculate tan and sin of values.
+
+    """    
     pass
 
 @function_group.command()
+@click.argument("name")
 @click.option(
     "-n", 
     "--number",
@@ -16,6 +20,11 @@ def function_group():
     help="Number of steps taken between 0 and 2*pi.",
     show_default=True)
 def sin(number):
+    """Calculate sine for values.
+
+    Calculate sine for values between 0 and 2pi 
+    in a certain amount of steps.
+    """    
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
@@ -28,6 +37,11 @@ def sin(number):
     help="Number of steps taken between 0 and 2*pi.",
     show_default=True)
 def tan(number):
+    """Calculate tangent for values.
+    
+    Calculate tangent for values between 0 and 2pi 
+    in a certain amount of steps.
+    """    
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
