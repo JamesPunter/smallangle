674194f0de741b1dbcef6a296985151f91653add
JamesPunter <james.punter@student.uva.nl>
2024-11-15 15:01:00 +0100

    CLI added to smallangle

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,15 +1,32 @@
 import click
 import numpy as np
-from numpy import pi
 import pandas as pd
+from numpy import pi
 
 
+@click.group()
+def function_group():
+    pass
+
+@function_group.command()
+@click.option(
+    "-n", 
+    "--number",
+    default=10,
+    help="Number of steps taken between 0 and 2*pi.",
+    show_default=True)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-
+@function_group.command()
+@click.option(
+    "-n", 
+    "--number",
+    default=10,
+    help="Number of steps taken between 0 and 2*pi.",
+    show_default=True)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -17,4 +34,4 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    function_group(10)
