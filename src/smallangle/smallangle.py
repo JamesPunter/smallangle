b442bf92ade0ba5261a527aa10f1d19f3ffc7bdb
JamesPunter <james.punter@student.uva.nl>
2024-11-15 16:05:20 +0100

    Update smallangle docstrings
    @click.argument("name") removed.

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -7,7 +7,6 @@ from numpy import pi
 @click.group()
 def function_group():
     """Calculate tan and sin of values.
-
     """    
     pass
 
@@ -16,9 +15,9 @@ def function_group():
 @click.option(
     "-n", 
     "--number",
-    default=10,
-    help="Number of steps taken between 0 and 2*pi.",
-    show_default=True)
+    default = 10,
+    help= "Number of steps taken between 0 and 2pi.",
+    show_default= True)
 def sin(number):
     """Calculate sine for values.
 
@@ -33,19 +32,18 @@ def sin(number):
 @click.option(
     "-n", 
     "--number",
-    default=10,
-    help="Number of steps taken between 0 and 2*pi.",
+    default = 10,
+    help= "Number of steps taken between 0 and 2pi.",
     show_default=True)
 def tan(number):
     """Calculate tangent for values.
-    
+
     Calculate tangent for values between 0 and 2pi 
     in a certain amount of steps.
     """    
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
-
 
 if __name__ == "__main__":
     function_group(10)
